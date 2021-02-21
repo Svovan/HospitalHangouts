f5d53ba7dfccf375ab46a810410da7ebf042bdef
Robb Tran <bobtran@usc.edu>
2021-02-20 21:58:07 -0800

    Beautiful code by a beautiful guy

--- hospital-hangout/src/chat/ChatRoom.js
+++ hospital-hangout/src/chat/ChatRoom.js
@@ -3,47 +3,75 @@ import firebase from '../firebase'
 import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 //general chat
-function ChatRoom() {
-    const dummy = useRef();
-    const messagesRef = firestore.collection('messages');
-    const query = messagesRef.orderBy('createdAt').limit(25);
+class ChatRoom extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dummy: '',
+            messages: [],
+            formValue: ''
+        }
+        this.dummy = React.createRef();
+        // this.messagesRef = firestore.collection('messages');
+        // this.query = this.messagesRef.orderBy('createdAt').limit(25);
+        this.sendMessage = this.sendMessage.bind(this)
+        this.update = this.update.bind(this)
+    }
+    componentDidMount() {
+        this.update();
+    }
+    update() {
+        firestore.collection('messages').orderBy('createdAt').limit(25).get()
+            .then( (snapshot) => {
+                snapshot.forEach(doc => {
+                    if (doc.exists) {
 
-    const [messages] = useCollectionData(query, {idField: 'id'});
-    
-    const [formValue, setFormValue] = useState('');
-    
-    const sendMessage = async(e) => {
-        e.preventDefault();
-        const { uid, photoURL } = auth.currentUser;
-        await messagesRef.add({
-            text: formValue,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-            uid,
-            photoURL
-        });
-        setFormValue('');
-        dummy.current.scrollIntoView({behavior: 'smooth'});
+                        this.setState(prevState => ({
+                            messages: [...prevState.messages, doc.data()]
+                        }));
+                        console.log(this.state.messages)
+                    }
+                    else {
+                        console.log("nothing for you man")
+                    }
+                })
+            })
+            .catch(err => console.log('Error ', err));
     }
 
-    return (
-        <>
-            <div>
-                {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
-                <div ref={dummy}> </div>
-            </div>
-            <form onSubmit={sendMessage}>
-               <input value={formValue} onChange={(e)=>setFormValue(e.target.value)}/> 
-               <button type="submit" disabled={!formValue}> </button>
-            </form>
-        </>
-    )
+    sendMessage = async(e) => {
+        e.preventDefault();
+        const { uid, photoURL } = auth.currentUser;
+        firestore.collection('messages').add({
+            text: this.state.formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL: '',
+        })
+        .then( () =>
+            this.setState({formValue: ''}) )
+        .then( () => this.dummy.current.scrollIntoView({behavior: 'smooth'}) );
+    }
+    render() {
+        return (
+            <>
+                <div>
+                    {this.state.messages && this.state.messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
+                    <div ref={this.dummy}> </div>
+                </div>
+                <form onSubmit={this.sendMessage}>
+                <input value={this.state.formValue} onChange={(e)=>this.setState({formValue: e.target.value})}/> 
+                <button type="submit" disabled={!this.state.formValue}> Submit Button </button>
+                </form>
+            </>
+        )
+    }
 }
 
 function ChatMessage(props) {
